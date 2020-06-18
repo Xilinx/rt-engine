b8610ecec9487e6c868899f8df0f66eb22eb652e
manasab <manasab@xilinx.com>
2020-06-17 19:01:56 -0700

    xclbin and meta.json for dpuv3int8

---
+++ controller/src/dpuv3int8/dpuv3int8_controller.cpp
@@ -0,0 +1,481 @@
+#include "dpuv3int8_controller.hpp"
+
+#include "utils.hpp"
+
+#include "experimental/xrt++.hpp"
+
+#include <typeinfo>
+
+#include <thread>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <iostream>
+#include <unistd.h>
+#include <sys/time.h>
+#include <ctime>
+#include <iomanip>
+#include <math.h>
+#include <cstdio>
+#include <mutex>
+
+
+#include <fstream>
+#include <sstream>
+#include <unistd.h>
+#include <unordered_map>
+
+#pragma GCC diagnostic push 
+#pragma GCC diagnostic ignored "-Wignored-qualifiers"
+#include "CL/cl_ext_xilinx.h"
+#pragma GCC diagnostic pop 
+#include "dpu_controller.hpp"
+#include "json-c/json.h"
+
+#include "device_handle.hpp"
+
+#define BATCH_SIZE  4
+#define SLR_NUM     4
+
+Dpuv3Int8Controller::Dpuv3Int8Controller(std::string meta) : XclDpuController(meta) {
+
+  // load meta file
+  std::ifstream f(meta);
+  std::stringstream metabuf;
+  metabuf << f.rdbuf();
+  json_object *jobj = json_tokener_parse(metabuf.str().c_str());     
+
+  // get kernel name
+  json_object *modelNameObj = NULL;
+  if (!json_object_object_get_ex(jobj, "model", &modelNameObj))
+    throw std::runtime_error("Error: missing 'model' field in meta.json");
+  modelName = json_object_get_string(modelNameObj);
+
+  json_object *instrNameObj = NULL;
+  if (!json_object_object_get_ex(jobj, "instrFile", &instrNameObj))
+    throw std::runtime_error("Error: missing 'instrFile' field in meta.json");
+  instr_filename = json_object_get_string(instrNameObj);
+
+  json_object *dInNameObj = NULL;
+  if (!json_object_object_get_ex(jobj, "dInFile", &dInNameObj))
+    throw std::runtime_error("Error: missing 'dInFile' field in meta.json");
+  din_filename = json_object_get_string(dInNameObj);
+
+  json_object *dOutNameObj = NULL;
+  if (!json_object_object_get_ex(jobj, "dOutFile", &dOutNameObj))
+    throw std::runtime_error("Error: missing 'dOutFile' field in meta.json");
+  dout_filename = json_object_get_string(dOutNameObj);
+
+  json_object *resultNameObj = NULL;
+  if (!json_object_object_get_ex(jobj, "resultFile", &resultNameObj))
+    throw std::runtime_error("Error: missing 'resultFile' field in meta.json");
+  result_filename = json_object_get_string(resultNameObj);
+
+  json_object *paramsNameObj = NULL;
+  if (!json_object_object_get_ex(jobj, "paramsFile", &paramsNameObj))
+    throw std::runtime_error("Error: missing 'paramsFile' field in meta.json");
+  params_filename = json_object_get_string(paramsNameObj);
+
+  init();
+}
+
+void Dpuv3Int8Controller::init()
+{
+  initializeTaskFUVariables();
+  initAllocateHostMemory();
+  initCreateBuffers();
+  initGetDevBufrAddr();
+  initBufrSize();
+}
+
+
+Dpuv3Int8Controller::~Dpuv3Int8Controller() {
+}
+void Dpuv3Int8Controller::run_Kernel(xrtcpp::exec::exec_write_command cmd, uint64_t* buf_addr, uint32_t* buf_size, uint32_t* reg_val)
+{
+
+    cmd.add(CONTROL_ADDR_BLOCK_INSTR            ,( buf_addr[BUF_IDX_INSTR]  ) & 0xFFFFFFFF);
+    cmd.add(CONTROL_ADDR_BLOCK_INSTR + 0x4      ,((buf_addr[BUF_IDX_INSTR]  ) >> 32) & 0xFFFFFFFF);
+    cmd.add(CONTROL_ADDR_BLOCK_PARAMS           ,( buf_addr[BUF_IDX_PARAMS] ) & 0xFFFFFFFF);
+    cmd.add(CONTROL_ADDR_BLOCK_PARAMS + 0x4     ,((buf_addr[BUF_IDX_PARAMS] ) >> 32) & 0xFFFFFFFF);
+    cmd.add(CONTROL_ADDR_BLOCK_SWAP             ,( buf_addr[BUF_IDX_SWAP]   ) & 0xFFFFFFFF);
+    cmd.add(CONTROL_ADDR_BLOCK_SWAP + 0x4       ,((buf_addr[BUF_IDX_SWAP]   ) >> 32) & 0xFFFFFFFF);
+    cmd.add(CONTROL_ADDR_BLOCK_RSLT             ,( buf_addr[BUF_IDX_RESULT] ) & 0xFFFFFFFF);
+    cmd.add(CONTROL_ADDR_BLOCK_RSLT + 0x4       ,((buf_addr[BUF_IDX_RESULT] ) >> 32) & 0xFFFFFFFF);
+    cmd.add(CONTROL_ADDR_BLOCK_PROF             ,( buf_addr[BUF_IDX_PROF]   ) & 0xFFFFFFFF);
+    cmd.add(CONTROL_ADDR_BLOCK_PROF + 0x4       ,((buf_addr[BUF_IDX_PROF]   ) >> 32) & 0xFFFFFFFF);
+    cmd.add(CONTROL_ADDR_BLOCK_SRC              ,( buf_addr[BUF_IDX_SRC]    ) & 0xFFFFFFFF);
+    cmd.add(CONTROL_ADDR_BLOCK_SRC + 0x4        ,((buf_addr[BUF_IDX_SRC]    ) >> 32) & 0xFFFFFFFF);
+    cmd.add(CONTROL_ADDR_BLOCK_DST              ,( buf_addr[BUF_IDX_DST]    ) & 0xFFFFFFFF);
+    cmd.add(CONTROL_ADDR_BLOCK_DST + 0x4        ,((buf_addr[BUF_IDX_DST]    ) >> 32) & 0xFFFFFFFF);
+
+    cmd.add(CONTROL_ADDR_TASK_FU_ADDR_STRD      ,reg_val[REG_IDX_TASK_FU_ADDR_STRD]  );
+    cmd.add(CONTROL_ADDR_TASK_FU_KW		        ,reg_val[REG_IDX_TASK_FU_KW]         );
+    cmd.add(CONTROL_ADDR_TASK_FU_SW		        ,reg_val[REG_IDX_TASK_FU_SW]         );
+    cmd.add(CONTROL_ADDR_TASK_FU_IC		        ,reg_val[REG_IDX_TASK_FU_IC]         );
+    cmd.add(CONTROL_ADDR_TASK_FU_OW		        ,reg_val[REG_IDX_TASK_FU_OW]         );
+    cmd.add(CONTROL_ADDR_TASK_FU_OH		        ,reg_val[REG_IDX_TASK_FU_OH]         );
+    cmd.add(CONTROL_ADDR_TASK_FU_SRC_NTRANS  	,reg_val[REG_IDX_TASK_FU_SRC_NTRANS] );
+    cmd.add(CONTROL_ADDR_TASK_FU_DST_NTRANS  	,reg_val[REG_IDX_TASK_FU_DST_NTRANS] );
+    cmd.add(CONTROL_ADDR_TASK_FU_PL_CORR		,reg_val[REG_IDX_TASK_FU_PL_CORR]    );
+    cmd.add(CONTROL_ADDR_TASK_FU_PR_CORR		,reg_val[REG_IDX_TASK_FU_PR_CORR]    );
+    cmd.add(CONTROL_ADDR_TASK_FU_IW_CORR		,reg_val[REG_IDX_TASK_FU_IW_CORR]    );
+    cmd.add(CONTROL_ADDR_TASK_FU_SW_CORR		,reg_val[REG_IDX_TASK_FU_SW_CORR]    );
+    cmd.add(CONTROL_ADDR_TASK_FU_WCG_CORR		,reg_val[REG_IDX_TASK_FU_WCG_CORR]   );
+    cmd.add(CONTROL_ADDR_TASK_FU_READ_MODE	    ,reg_val[REG_IDX_TASK_FU_READ_MODE]  );
+    cmd.add(CONTROL_ADDR_TASK_MODE		        ,reg_val[REG_IDX_TASK_MODE]          );
+    cmd.add(CONTROL_ADDR_REG_VERSION		    ,reg_val[REG_IDX_REG_VERSION]        );
+    cmd.add(CONTROL_ADDR_REG_AXCACHE_AXOS		,reg_val[REG_IDX_REG_AXCACHE_AXOS]   );
+    cmd.add(CONTROL_ADDR_REG_DPU_PROF_ENABLE	,reg_val[REG_IDX_REG_DPU_PROF_ENABLE]);
+    cmd.add(CONTROL_ADDR_REG_DPU_PROF_FLAG	    ,reg_val[REG_IDX_REG_DPU_PROF_FLAG]  );
+    cmd.add(CONTROL_ADDR_REG_DPU_PROF_TSTAMP	,reg_val[REG_IDX_REG_DPU_PROF_TSTAMP]);
+    cmd.add(CONTROL_ADDR_REG_DPU_PROF_LSNUM	    ,reg_val[REG_IDX_REG_DPU_PROF_LSNUM] );
+    cmd.add(CONTROL_ADDR_REG_DPU_PROF_SSNUM	    ,reg_val[REG_IDX_REG_DPU_PROF_SSNUM] );
+    cmd.add(CONTROL_ADDR_REG_DPU_PROF_CSNUM	    ,reg_val[REG_IDX_REG_DPU_PROF_CSNUM] );
+    cmd.add(CONTROL_ADDR_REG_DPU_PROF_MSNUM	    ,reg_val[REG_IDX_REG_DPU_PROF_MSNUM] );
+    cmd.add(CONTROL_ADDR_REG_DPU_PROF_LFNUM	    ,reg_val[REG_IDX_REG_DPU_PROF_LFNUM] );
+    cmd.add(CONTROL_ADDR_REG_DPU_PROF_SFNUM	    ,reg_val[REG_IDX_REG_DPU_PROF_SFNUM] );
+    cmd.add(CONTROL_ADDR_REG_DPU_PROF_CFNUM	    ,reg_val[REG_IDX_REG_DPU_PROF_CFNUM] );
+    cmd.add(CONTROL_ADDR_REG_DPU_PROF_MFNUM	    ,reg_val[REG_IDX_REG_DPU_PROF_MFNUM] );
+    cmd.add(CONTROL_ADDR_REG_DPU_STATUS		    ,reg_val[REG_IDX_REG_DPU_STATUS]     );
+    cmd.add(CONTROL_ADDR_REG_FU_STATUS		    ,reg_val[REG_IDX_REG_FU_STATUS]      );
+    cmd.add(CONTROL_ADDR_DONE_CNT		        ,reg_val[REG_IDX_DONE_CNT]           );
+
+    cmd.execute();
+    cmd.wait();
+
+}
+
+void Dpuv3Int8Controller::initializeTaskFUVariables()
+{
+    std::cout<<"-----------------------------------------------"<<std::endl;
+    std::cout << "Program begins: "<< std::endl;
+
+    if(modelName=="res50")
+    {
+        task_fu_addr_strd = 0x24c00;
+        task_fu_kw = 0x6;
+        task_fu_sw = 0x1;
+        task_fu_ic = 0x2;
+        task_fu_ow = 0xe4;
+        task_fu_oh = 0xdf;
+        task_fu_src_ntrans = 0x931;
+        task_fu_dst_ntrans = 0xc400;
+        task_fu_pl_corr = 0x9;
+        task_fu_pr_corr = 0x2a9;
+        task_fu_iw_corr = 224*3;   //need update
+        task_fu_sw_corr = 6;   //need update
+        task_fu_wcg_corr = 0xe0;
+        task_fu_read_mode = 0x2;
+        task_mode = 0x0;
+        reg_axcache_axos = 0x01012020;
+        reg_dpu_prof_enable = 0x1;
+    }
+    else if(modelName=="res34")
+    {
+        task_fu_addr_strd = 0x24c00;
+        task_fu_kw = 0x6;
+        task_fu_sw = 0x1;
+        task_fu_ic = 0x2;
+        task_fu_ow = 0xe4;
+        task_fu_oh = 0xdf;
+        task_fu_src_ntrans = 0x931;
+        task_fu_dst_ntrans = 0xc400;
+        task_fu_pl_corr = 0x9;
+        task_fu_pr_corr = 0x2a9;
+        task_fu_iw_corr = 224*3;
+        task_fu_sw_corr = 6;
+        task_fu_wcg_corr = 0xe0;
+        task_fu_read_mode = 0x2;
+        task_mode = 0x1;
+        reg_axcache_axos = 0x01012020;
+        reg_dpu_prof_enable = 0x1;
+    }
+    
+    uint32_t zero = 0;
+
+    //Initialize register value
+    reg_val[REG_IDX_TASK_FU_ADDR_STRD]      = task_fu_addr_strd;
+    reg_val[REG_IDX_TASK_FU_KW]             = task_fu_kw;
+    reg_val[REG_IDX_TASK_FU_SW]             = task_fu_sw;
+    reg_val[REG_IDX_TASK_FU_IC]             = task_fu_ic;
+    reg_val[REG_IDX_TASK_FU_OW]             = task_fu_ow;
+    reg_val[REG_IDX_TASK_FU_OH]             = task_fu_oh;
+    reg_val[REG_IDX_TASK_FU_SRC_NTRANS]     = task_fu_src_ntrans;
+    reg_val[REG_IDX_TASK_FU_DST_NTRANS]     = task_fu_dst_ntrans;
+    reg_val[REG_IDX_TASK_FU_PL_CORR]        = task_fu_pl_corr;
+    reg_val[REG_IDX_TASK_FU_PR_CORR]        = task_fu_pr_corr;
+    reg_val[REG_IDX_TASK_FU_IW_CORR]        = task_fu_iw_corr;
+    reg_val[REG_IDX_TASK_FU_SW_CORR]        = task_fu_sw_corr;
+    reg_val[REG_IDX_TASK_FU_WCG_CORR]       = task_fu_wcg_corr;
+    reg_val[REG_IDX_TASK_FU_READ_MODE]      = task_fu_read_mode;
+    reg_val[REG_IDX_TASK_MODE]              = task_mode;
+    reg_val[REG_IDX_REG_VERSION]            = zero;
+    reg_val[REG_IDX_REG_AXCACHE_AXOS]       = reg_axcache_axos;
+    reg_val[REG_IDX_REG_DPU_PROF_ENABLE]    = reg_dpu_prof_enable;
+    reg_val[REG_IDX_REG_DPU_PROF_FLAG]      = zero;
+    reg_val[REG_IDX_REG_DPU_PROF_TSTAMP]    = zero;
+    reg_val[REG_IDX_REG_DPU_PROF_LSNUM]     = zero;
+    reg_val[REG_IDX_REG_DPU_PROF_SSNUM]     = zero;
+    reg_val[REG_IDX_REG_DPU_PROF_CSNUM]     = zero;
+    reg_val[REG_IDX_REG_DPU_PROF_MSNUM]     = zero;
+    reg_val[REG_IDX_REG_DPU_PROF_LFNUM]     = zero;
+    reg_val[REG_IDX_REG_DPU_PROF_SFNUM]     = zero;
+    reg_val[REG_IDX_REG_DPU_PROF_CFNUM]     = zero;
+    reg_val[REG_IDX_REG_DPU_PROF_MFNUM]     = zero;
+    reg_val[REG_IDX_REG_DPU_STATUS]         = zero;
+    reg_val[REG_IDX_REG_FU_STATUS]          = zero;
+    reg_val[REG_IDX_DONE_CNT]               = zero;
+
+
+}
+
+void Dpuv3Int8Controller::initAllocateHostMemory()
+{
+    //Allocate Memory in Host Memory
+    uint32_t BLK_SIZE = 16*1024*1024;
+
+    instr=load(instr_filename);
+    params=load(params_filename);
+    swap.resize(BLK_SIZE/sizeof(int32_t));
+    fuSrc.resize(BLK_SIZE/sizeof(int32_t));
+    fuDst.resize(BLK_SIZE/sizeof(int32_t));       	    
+    prof.resize(BLK_SIZE/sizeof(int32_t));
+    
+    std::cout<<"One time initialzation"<<std::endl;
+}
+
+
+void Dpuv3Int8Controller::runAllocateHostMemory()
+{
+    //Allocate Memory in Host Memory
+    uint32_t BLK_SIZE = 16*1024*1024;
+
+    din=load(din_filename);
+    dout=load(dout_filename);
+    result.resize(dout.size());
+    
+    std::cout<<"Run time initialzation for each query"<<std::endl;
+
+}
+
+void Dpuv3Int8Controller::initCreateBuffers()
+{
+    std::cout << "Create Program and Kernel" << std::endl;
+    //OPENCL HOST CODE AREA START
+    // Use the first Xilinx device found in the system
+    cl_context context = handle_.get_context();
+    cl_int err = CL_SUCCESS;
+    static const std::vector<unsigned> ddrBankMap = {
+     XCL_MEM_DDR_BANK0,
+     XCL_MEM_DDR_BANK1,
+     XCL_MEM_DDR_BANK2,
+     XCL_MEM_DDR_BANK3
+    };
+    cl_mem_flags flags = CL_MEM_USE_HOST_PTR | CL_MEM_READ_WRITE;
+    
+
+    void *instrhost_ptr = (void*)instr.data();
+    size_t instrsize = instr.size()*sizeof(uint32_t);
+    instr_buf = new XclDeviceBuffer(handle_, instrhost_ptr, instrsize, ddrBankMap[handle_.get_device_info().ddr_bank], flags);
+
+
+    void *paramshost_ptr = (void*)params.data();
+    size_t paramssize = params.size()*sizeof(uint32_t);
+    params_buf = new XclDeviceBuffer(handle_, paramshost_ptr, paramssize, ddrBankMap[handle_.get_device_info().ddr_bank], flags);
+
+    void *swaphost_ptr = (void*)swap.data();
+    size_t swapsize = swap.size()*sizeof(uint32_t);
+    swap_buf = new XclDeviceBuffer(handle_, swaphost_ptr, swapsize, ddrBankMap[handle_.get_device_info().ddr_bank], flags);
+
+    void *fuSrchost_ptr = (void*)fuSrc.data();
+    size_t fuSrcsize = fuSrc.size()*sizeof(uint32_t);
+    fuSrc_buf = new XclDeviceBuffer(handle_, fuSrchost_ptr, fuSrcsize, ddrBankMap[handle_.get_device_info().ddr_bank], flags);
+
+    void *fuDsthost_ptr = (void*)fuDst.data();
+    size_t fuDstsize = fuDst.size()*sizeof(uint32_t);
+    fuDst_buf = new XclDeviceBuffer(handle_, fuDsthost_ptr, fuDstsize, ddrBankMap[handle_.get_device_info().ddr_bank], flags);
+
+    void *profhost_ptr = (void*)prof.data();
+    size_t profsize = prof.size()*sizeof(uint32_t);
+    prof_buf = new XclDeviceBuffer(handle_, profhost_ptr, profsize, ddrBankMap[handle_.get_device_info().ddr_bank], flags);
+}
+
+
+
+void Dpuv3Int8Controller::runCreateBuffers()
+{
+    std::cout << "Create Program and Kernel" << std::endl;
+    //OPENCL HOST CODE AREA START
+    // Use the first Xilinx device found in the system
+    cl_context context = handle_.get_context();
+    cl_int err = CL_SUCCESS;
+    static const std::vector<unsigned> ddrBankMap = {
+     XCL_MEM_DDR_BANK0,
+     XCL_MEM_DDR_BANK1,
+     XCL_MEM_DDR_BANK2,
+     XCL_MEM_DDR_BANK3
+    };
+    cl_mem_flags flags = CL_MEM_USE_HOST_PTR | CL_MEM_READ_WRITE;
+    
+
+    void *host_ptr = (void*)din.data();
+    size_t size = din.size()*sizeof(uint32_t);
+    din_buf = new XclDeviceBuffer(handle_, host_ptr, size, ddrBankMap[handle_.get_device_info().ddr_bank], flags);
+    
+    void *resulthost_ptr = (void*)result.data();
+    size_t resultsize = result.size()*sizeof(uint32_t);
+    result_buf = new XclDeviceBuffer(handle_, resulthost_ptr, resultsize, ddrBankMap[handle_.get_device_info().ddr_bank], flags);
+
+}
+
+void Dpuv3Int8Controller::initGetDevBufrAddr()
+{
+    //Get device buffer address 
+    
+    buf_addr[BUF_IDX_INSTR] = instr_buf->get_phys_addr();
+    buf_addr[BUF_IDX_PARAMS] = params_buf->get_phys_addr();
+    buf_addr[BUF_IDX_SWAP] = swap_buf->get_phys_addr();
+
+    buf_addr[BUF_IDX_FUSRC] = fuSrc_buf->get_phys_addr();
+    buf_addr[BUF_IDX_FUDST] = fuDst_buf->get_phys_addr();
+    buf_addr[BUF_IDX_PROF] = prof_buf->get_phys_addr();
+
+}
+
+void Dpuv3Int8Controller::runGetDevBufrAddr()
+{
+    //Get device buffer address 
+    
+    buf_addr[BUF_IDX_DIN] = din_buf->get_phys_addr();
+    buf_addr[BUF_IDX_RESULT] = result_buf->get_phys_addr();
+
+}
+
+
+
+void Dpuv3Int8Controller::initBufrSize()
+{
+    //Initialize buf_size
+    buf_size[BUF_IDX_INSTR]     = instr.size()*sizeof(uint32_t);
+    buf_size[BUF_IDX_PARAMS]    = params.size()*sizeof(uint32_t);
+    buf_size[BUF_IDX_SWAP]      = swap.size()*sizeof(uint32_t);
+    buf_size[BUF_IDX_FUSRC]     = fuSrc.size()*sizeof(uint32_t);
+    buf_size[BUF_IDX_FUDST]     = fuDst.size()*sizeof(uint32_t);
+    buf_size[BUF_IDX_PROF]      = prof.size()*sizeof(uint32_t);
+    buf_size[BUF_IDX_SRC]       = 0;
+    buf_size[BUF_IDX_DST]       = 0;
+    buf_size[BUF_IDX_NULL]      = 0;
+
+    if(task_mode == 0) // If FU is used 
+    {
+        buf_addr[BUF_IDX_DST] = buf_addr[BUF_IDX_FUDST];
+        buf_size[BUF_IDX_DST] = buf_size[BUF_IDX_FUDST];
+    }                                                                 
+    else            // If FU is NOT used                                                      
+    {                                                                 
+        buf_addr[BUF_IDX_SRC] = buf_addr[BUF_IDX_NULL];
+        buf_size[BUF_IDX_SRC] = buf_size[BUF_IDX_NULL];
+    }
+
+}
+
+void Dpuv3Int8Controller::runInitBufrSize()
+{
+    //Initialize buf_size
+    buf_size[BUF_IDX_DIN]       = din.size()*sizeof(uint32_t);
+    buf_size[BUF_IDX_DOUT]      = dout.size()*sizeof(uint32_t);
+    buf_size[BUF_IDX_RESULT]    = result.size()*sizeof(uint32_t);
+
+    if(task_mode == 0) // If FU is used 
+    {
+        buf_addr[BUF_IDX_SRC] = buf_addr[BUF_IDX_DIN];
+        buf_size[BUF_IDX_SRC] = buf_size[BUF_IDX_DIN];
+    }                                                                 
+    else            // If FU is NOT used                                                      
+    {                                                                 
+        buf_addr[BUF_IDX_DST] = buf_addr[BUF_IDX_DIN];
+        buf_size[BUF_IDX_DST] = buf_size[BUF_IDX_DIN];
+    }
+}
+
+
+
+
+void Dpuv3Int8Controller::execute()
+{
+    xrt_device* xdev = handle_.get_device_info().xrt_dev;
+    xrtcpp::acquire_cu_context(xdev,0);
+    xrtcpp::exec::exec_write_command cmd(xdev);
+    cmd.add_cu(0);
+    auto start = utils::time_ns();
+    std::cout << "Launch the kernel" << std::endl;
+    run_Kernel( cmd, buf_addr, buf_size, reg_val);
+    auto end = utils::time_ns();
+    std::cout << "All Done !" <<  " | time (ms): " << (end-start)*1e-6 << "| fps: " << float(1.0*1e9/(end-start))*BATCH_SIZE*SLR_NUM << std::endl;
+    xrtcpp::release_cu_context(xdev,0);
+
+}
+
+void Dpuv3Int8Controller::checkFpgaOutput()
+{
+    std::ofstream outputfile;
+    std::string log_filename="";
+    for (unsigned i = 0 ; i < dout.size(); i++)
+    {
+
+        if(i%(result.size())==0)
+        {
+            log_filename = "result" + std::to_string(25 + ( std::rand() % ( 63 - 25 + 1 ) )) + ".txt";
+//           log_filename = "result" + std::to_string(i/(result.size())) + ".txt";
+            outputfile.open (log_filename);
+        }
+
+        if ((uint32_t)dout[i] != (uint32_t)result.data()[i])
+        {
+            outputfile << "Error: Result mismatch.  " << "dout: " << std::hex << std::setw(8) << std::setfill('0') << (uint32_t)dout[i] << " | result: " << std::hex << std::setw(8) << (uint32_t)result.data()[i] << std::endl;
+        }
+        else 
+        {
+            outputfile << std::hex << std::setw(8) << std::setfill('0') << (uint32_t)result.data()[i] << std::endl;
+        }
+
+        if((i%(result.size()))==((result.size())-1))
+        {
+            outputfile.close();
+        }
+    }
+
+}
+
+void Dpuv3Int8Controller::run(const std::vector<xir::vart::TensorBuffer*> &inputs, 
+                        const std::vector<xir::vart::TensorBuffer*> &outputs) {
+    runAllocateHostMemory();
+    runCreateBuffers();
+    runGetDevBufrAddr();
+    runInitBufrSize();
+    execute();
+    download(result_buf);
+    checkFpgaOutput();
+
+}
+
+std::vector<int32_t, aligned_allocator<int32_t>> Dpuv3Int8Controller::load(std::string filename)
+{
+    std::vector<int32_t, aligned_allocator<int32_t>> tmp;
+    std::vector<int32_t, aligned_allocator<int32_t>> mem;
+    std::ifstream   ifile;
+    std::string     line;
+    
+    ifile.open(filename);
+    while(std::getline(ifile, line))
+    {
+        tmp.push_back((int32_t)std::strtol(line.c_str(), nullptr, 16));
+    }
+    ifile.close();
+
+    mem.insert(mem.end(), tmp.begin(), tmp.end());
+
+    return mem;
+}
+
