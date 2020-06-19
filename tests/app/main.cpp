58d93d68dbb208df47a3e6af40bc5edcb2236dec
Aaron <aaronn@xilinx.com>
2020-06-19 11:48:46 -0700

    add cxxopts cmdline parsing for tests main.cpp

--- tests/app/main.cpp
+++ tests/app/main.cpp
@@ -1,16 +1,28 @@
 // Vitis API execute_async example
+#include <algorithm>
 #include <chrono>
 #include <iostream>
+#include "cxxopts.hpp"
 #include "tests.hpp"
 
 int main(int argc, char** argv) {
-  if (argc < 2) 
-    throw std::runtime_error("Usage: app.exe path_to_meta.json");
+  cxxopts::Options options("AppTests", "Vitis application-level tests");
+  options.add_options()
+    ("r,runnermeta", "Path to DpuRunner meta.json", cxxopts::value<std::string>())
+    ("t,tests", "Tests to run", cxxopts::value<std::vector<std::string>>()->default_value("st,mtsr,mtmr"))
+    ;
+  auto result = options.parse(argc, argv);
+  if (!result.count("runnermeta"))
+    throw std::runtime_error("Usage: app.exe -r path_to_runner_meta.json");
 
+  const std::vector<std::string> tests = result["tests"].as<std::vector<std::string>>();
+  const auto runnerMeta = result["runnermeta"].as<std::string>();
+
+  if (std::find(tests.begin(), tests.end(), "st") != tests.end())
   {
     const unsigned numQueries = 10000;
     std::cout << std::endl << "Testing single thread..." << std::endl;
-    SingleThreadTest singleThreadTest(argv[1], numQueries);
+    SingleThreadTest singleThreadTest(runnerMeta, numQueries);
     auto t1 = std::chrono::high_resolution_clock::now();
     singleThreadTest.run();
     auto t2 = std::chrono::high_resolution_clock::now();
@@ -19,11 +31,12 @@ int main(int argc, char** argv) {
     std::cout << "QPS: " << numQueries/elapsed.count() << std::endl;
   }
 
+  if (std::find(tests.begin(), tests.end(), "mtsr") != tests.end())
   {
     const unsigned numQueries = 10000;
     const unsigned numThreads = 100;
     std::cout << std::endl << "Testing multi thread (single runner)..." << std::endl;
-    MultiThreadTest multiThreadTest(argv[1], numQueries, numThreads, /*numRunners*/1);
+    MultiThreadTest multiThreadTest(runnerMeta, numQueries, numThreads, /*numRunners*/1);
     auto t1 = std::chrono::high_resolution_clock::now();
     multiThreadTest.run();
     auto t2 = std::chrono::high_resolution_clock::now();
@@ -32,12 +45,13 @@ int main(int argc, char** argv) {
     std::cout << "QPS: " << numQueries/elapsed.count() << std::endl;
   }
 
+  if (std::find(tests.begin(), tests.end(), "mtmr") != tests.end())
   {
     const unsigned numQueries = 10000;
     const unsigned numThreads = 100; 
     const unsigned numRunners = 2; 
     std::cout << std::endl << "Testing multi thread (multi runner)..." << std::endl;
-    MultiThreadTest multiThreadTest(argv[1], numQueries, numThreads, numRunners);
+    MultiThreadTest multiThreadTest(runnerMeta, numQueries, numThreads, numRunners);
     auto t1 = std::chrono::high_resolution_clock::now();
     multiThreadTest.run();
     auto t2 = std::chrono::high_resolution_clock::now();
