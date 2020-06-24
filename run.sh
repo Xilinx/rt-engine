884e9e48b3ed17458c21dfd8102d5a183a37d3c1
manasab <manasab@xilinx.com>
2020-06-23 18:11:06 -0700

    adding run script for dpuv3int8, can be extended to any IP

---
+++ run.sh
@@ -0,0 +1,9 @@
+export LD_LIBRARY_PATH=build:${CONDA_PREFIX}/lib:/opt/xilinx/xrt/lib
+export XILINX_XRT=/opt/xilinx/xrt
+
+#Sample commands
+#build/tests/engine.exe
+#build/tests/app.exe tests/app/models/xdnn_resnet50/meta.json
+
+#dpuv3int8 command
+build/tests/app.exe -r tests/app/models/dpuv3int8_resnet50/meta.json
