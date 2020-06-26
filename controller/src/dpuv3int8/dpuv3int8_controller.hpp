6871210e0f1c0d4a454844044bc8bce028b6fafb
manasab <manasab@xilinx.com>
2020-06-26 12:49:51 -0700

    few cleanups for names

--- controller/src/dpuv3int8/dpuv3int8_controller.hpp
+++ controller/src/dpuv3int8/dpuv3int8_controller.hpp
@@ -28,31 +28,26 @@ class Dpuv3Int8Controller : public XclDpuController<XclDeviceHandle, XclDeviceBu
   virtual ~Dpuv3Int8Controller() override;
   virtual void run(const std::vector<xir::vart::TensorBuffer*> &inputs, 
            const std::vector<xir::vart::TensorBuffer*> &outputs) override;
-  std::vector<int32_t, aligned_allocator<int32_t>> load(std::string filename);
 
   virtual std::vector<const xir::vart::Tensor*> get_input_tensors() const override; 
   virtual std::vector<const xir::vart::Tensor*> get_output_tensors() const override; 
   virtual std::vector<xir::vart::TensorBuffer*> get_inputs() override;
   virtual std::vector<xir::vart::TensorBuffer*> get_outputs() override;
-  std::vector<xir::vart::TensorBuffer*> create_hw_buffers(std::vector<xir::vart::TensorBuffer*> stdBuf, bool isInput=true);
-  xir::vart::TensorBuffer* get_hw_buffer(xir::vart::TensorBuffer *tb);
 
 
  private:    
-
-  void preprocess(xir::vart::TensorBuffer*, xir::vart::TensorBuffer*);//TO-DO MNDBG
-  void postprocess(xir::vart::TensorBuffer*, xir::vart::TensorBuffer*);//TO_DO MNDBG
-  std::unordered_map<xir::vart::TensorBuffer*, xir::vart::TensorBuffer*> stdbuf2hwbuf_;
-
+  
   void initializeTaskFUVariables();//TO-DO MNDBG 
   void initCreateBuffers();//TO-DO MNDBG
+  void preprocess(xir::vart::TensorBuffer*, xir::vart::TensorBuffer*);//TO-DO MNDBG
+  void postprocess(xir::vart::TensorBuffer*, xir::vart::TensorBuffer*);//TO_DO MNDBG
+  std::vector<xir::vart::TensorBuffer*> create_hw_buffers(std::vector<xir::vart::TensorBuffer*> stdBuf, bool isInput=true);
+  std::vector<int32_t, aligned_allocator<int32_t>> load(std::string filename);
+  xir::vart::TensorBuffer* get_hw_buffer(xir::vart::TensorBuffer *tb);
   void initRunBufs(uint64_t *buf_addr, uint32_t *buf_size);
-  
   void execute(uint64_t *buf_addr, uint32_t *buf_size);
-  void run_Kernel(xrtcpp::exec::exec_write_command cmd, uint64_t* buf_addr, uint32_t* buf_size, uint32_t* reg_val);
+  void runKernel(xrtcpp::exec::exec_write_command cmd, uint64_t* buf_addr, uint32_t* buf_size, uint32_t* reg_val);
   void checkFpgaOutput(XclDeviceBuffer *outbuf);
-  
-  std::unique_ptr<XclDeviceBuffer> createBuffer(void* hostPtr, size_t size);
   std::string getFileNameIfExists(std::string name, json_object* jobj);
 
   std::unique_ptr<xir::vart::Tensor> in_tensor_;
@@ -64,6 +59,8 @@ class Dpuv3Int8Controller : public XclDpuController<XclDeviceHandle, XclDeviceBu
   std::unique_ptr<xir::vart::Tensor> swap_tensor_;
   std::unique_ptr<xir::vart::Tensor> fuSrc_tensor_;
   std::unique_ptr<xir::vart::Tensor> fuDst_tensor_;
+
+  std::unordered_map<xir::vart::TensorBuffer*, xir::vart::TensorBuffer*> stdbuf2hwbuf_;
 
   std::vector<int,aligned_allocator<int>> dout_;
  
