9de6d445d0c8161248e1fb811c3e9b901831aaf3
Aaron <aaronn@xilinx.com>
2020-09-16 13:28:23 -0700

    remove unused

--- run.sh
+++ run.sh
@@ -4,7 +4,3 @@ export XILINX_XRT=/opt/xilinx/xrt
 #Sample commands
 #build/tests/engine.exe
 build/tests/app.exe -r tests/app/models/sample_resnet50/meta.json
-
-#dpuv3int8 command
-#export DPUV3INT8_DEBUG_MODE=1
-#build/tests/app.exe -r tests/app/models/dpuv3int8_resnet50/meta.json -t tc
