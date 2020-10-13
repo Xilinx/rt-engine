4f2a7791da99620b27d1b1bd61c886eb6364b660
anup <anup@xilinx.com>
2020-10-12 22:18:59 -0700

    move vart sample to tests

--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
-export LD_LIBRARY_PATH=build:${CONDA_PREFIX}/lib:/opt/xilinx/xrt/lib
+
+# vart, xir & Unilog lib dir
+PREFIX=${HOME}/.local/Ubuntu.18.04.x86_64
+
+if [ -z $1 ];
+then
+	# setting default path
+	XILINX_VART=$PREFIX.Release
+else
+    XILINX_VART=$1
+fi
+
+export LD_LIBRARY_PATH=build:${CONDA_PREFIX}/lib:/opt/xilinx/xrt/lib:${XILINX_VART}/lib
 export XILINX_XRT=/opt/xilinx/xrt
 
-#Sample commands
+# dpuv3int8 - vart resnet50 sample
+./build/tests/vart_dpuv3int8.exe tests/dpuv3int8/models/dpuv3int8_xir/resnet50_handwritten.xmodel tests/dpuv3int8/models/dpuv3int8_xir/meta.json tests/dpuv3int8/models/commonImgLabelDir/imageDir/
+
 #build/tests/engine.exe
-build/tests/app.exe -r tests/app/models/sample_resnet50/meta.json
--- vart_samples/dpuv3int8/resnet50/run.sh
+++
@@ -1,8 +0,0 @@
-
-export LD_LIBRARY_PATH=build:${CONDA_PREFIX}/lib:/opt/xilinx/xrt/lib
-export XILINX_XRT=/opt/xilinx/xrt
-
-export DPUV3INT8_DEBUGMODE=0
-cd ../../../
-./vart_samples/dpuv3int8/resnet50/resnet50 tests/dpuv3int8/models/dpuv3int8_xir/resnet50_handwritten.xmodel tests/dpuv3int8/models/dpuv3int8_xir/meta.json tests/dpuv3int8/models/commonImgLabelDir/imageDir/
-cd -
