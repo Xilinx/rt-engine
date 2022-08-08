a6b83fb8202d31360c26fca5d9a2076fe7598829
sowmyaka <sowmyaka@xilinx.com>
2022-08-08 11:46:18 -0700

    More cleanup, remove unused code

--- controller/src/flexml/flexml_controller.cpp
+++ controller/src/flexml/flexml_controller.cpp
@@ -22,32 +22,13 @@
 #include <limits>
 #include <fstream>
 typedef unsigned int uint;
-DEF_ENV_PARAM(DEBUG_DPU_CONTROLLER, "1")
+DEF_ENV_PARAM(DEBUG_DPU_CONTROLLER, "0")
 
 namespace {
   // Convert Set to Vector
   std::vector<const xir::Tensor *> initTensorVec(const std::set<const xir::Tensor *> &tensorSet) {
     return {tensorSet.cbegin(), tensorSet.cend()};
   }
-
-  // Convert float to int8 with rounding
-  inline std::int8_t float2fix(float data) {
-    static const int data_max = std::numeric_limits<std::int8_t>::max();
-    static const int data_min = std::numeric_limits<std::int8_t>::min();
-    float rlt;
-    if (data > data_max) {
-      rlt = data_max;
-    } else if (data < data_min) {
-      rlt = data_min;
-    } else if (data < 0 && (data - floor(data)) == 0.5f) {
-      rlt = std::ceil(data);
-    } else {
-      rlt = std::round(data);
-    }
-    return rlt;
-  }
-
-}
 
 xrt::uuid load_xclbin(xrt::device device){
   LOG_IF(INFO, ENV_PARAM(DEBUG_DPU_CONTROLLER))
@@ -76,17 +57,14 @@ FlexmlController::FlexmlController(const xir::Subgraph *subgraph, unsigned int d
     {
 
 
- // load ucode
-  std::cout << "load ucode" << std::endl;
 
     // FIXME use TinyYolo data
     int group_id = pl_ctrl_sw_.getGroupId();
-    std::cout << "group_id=" << group_id << std::endl;
+    LOG_IF(INFO, ENV_PARAM(DEBUG_DPU_CONTROLLER)) << "group_id=" << group_id ;
 
 
   // Extract key XMODEL info
   for (auto& inTensor : inTensors_) {
-    //inputScales_.emplace_back(pow(2,inTensor->get_attr<std::int32_t>("fix_point")));
     paddedInputShape_.emplace_back(inTensor->get_shape());
     auto data_op = inTensor->get_producer();
     if(data_op->has_attr("origin_input_pad") && data_op->has_attr("origin_input_shape")) {
@@ -117,12 +95,9 @@ FlexmlController::FlexmlController(const xir::Subgraph *subgraph, unsigned int d
       << "Padding: ";
     for (auto& el : padding_.back())
       LOG_IF(INFO, ENV_PARAM(DEBUG_DPU_CONTROLLER)) << " " << el;
-    //LOG_IF(INFO, ENV_PARAM(DEBUG_DPU_CONTROLLER))
-      //<< "Scale Factor: " << inputScales_.back();
 
   }
   for (auto& outTensor : outTensors_) {
-    //outputScales_.emplace_back(pow(2,-1*outTensor->get_attr<std::int32_t>("fix_point")));
     paddedOutputShape_.emplace_back(outTensor->get_shape());
     auto download_op = outTensor->get_producer();
     auto data_op = download_op->get_input_op("input");
@@ -148,14 +123,11 @@ FlexmlController::FlexmlController(const xir::Subgraph *subgraph, unsigned int d
     for (auto& el : paddedOutputShape_.back())
       LOG_IF(INFO, ENV_PARAM(DEBUG_DPU_CONTROLLER)) << " " << el;
 
-    //LOG_IF(INFO, ENV_PARAM(DEBUG_DPU_CONTROLLER))
-     // << "Scale Factor: " << outputScales_.back();
   }
 
   // Load instructions from xmodel
   LOG_IF(INFO, ENV_PARAM(DEBUG_DPU_CONTROLLER))
     << "Loading Model Instructions from XMODEL";
-  std::cout << "Loading Model Instructions from XMODEL" <<std::endl;
   auto mc_code = subgraph->get_attr<std::vector<char>>("mc_code");
   std::vector<uint8_t> xmodel_ucode;
   for(char byte:mc_code){
@@ -165,7 +137,6 @@ FlexmlController::FlexmlController(const xir::Subgraph *subgraph, unsigned int d
   pl_ctrl_sw_.loadMicroCode(xmodel_ucode);
   LOG_IF(INFO, ENV_PARAM(DEBUG_DPU_CONTROLLER))
     << "Finished Loading Model Instructions from XMODEL";
-  std::cout << "Finished Loading Model Instructions from XMODEL" <<std::endl;
   
   // Load weights and weight offsets from xmodel 
   LOG_IF(INFO, ENV_PARAM(DEBUG_DPU_CONTROLLER))
@@ -205,8 +176,6 @@ FlexmlController::FlexmlController(const xir::Subgraph *subgraph, unsigned int d
       inputBuffers_[i].emplace_back(
         device_, inTensor->get_data_size(),group_id
       );
-        std::cout << "size of input buffers" <<  inputBuffers_.size() << std::endl;
-  std::cout << " size of input buffers" << inputBuffers_[0].size() << std::endl;
       pl_ctrl_sw_.setAddress("compute_graph.ifm_ddr",inputBuffers_[i][inTensorCount].address());
       inTensorCount++;
     }
@@ -224,23 +193,11 @@ FlexmlController::FlexmlController(const xir::Subgraph *subgraph, unsigned int d
 }
 
 void FlexmlController::copyInputs(const unsigned int wIdx, const std::vector<vart::TensorBuffer*> &inputs) {
-  std::cout << " Number of tensor buffers = " << inputs.size() << std::endl;
   for (unsigned i = 0; i < inputs.size(); i++) {
   // TODO: Don't map XRT BO iteratively, do this statically in constructor
     std::memcpy(inputBuffers_[wIdx][i].map<void*>(), reinterpret_cast<void *>(inputs[i]->data().first), inputs[i]->data().second);
-    //if(i == 0)
-    //  std::cout << inputs[i]->to_string() << std::endl;
     // Sync To Device.
     inputBuffers_[wIdx][i].sync(XCL_BO_SYNC_BO_TO_DEVICE);
-    // Debug 
-    auto ifm_bo_map = inputBuffers_[wIdx][i].map<uint32_t*>();
-    std::ofstream debugFile("inputs_from_VTB.txt");
-
-    for (unsigned int index=0;index<inputs[i]->data().second /4 ;index++){
-        debugFile << std::hex << ifm_bo_map[index] << std::endl;
-    }
-    //std::cout << "Inputs byte" << i << "from bo map:" << ifm_bo_map << std::endl;
-    debugFile.close();
   }
 }
 
@@ -259,23 +216,21 @@ void FlexmlController::run(const std::vector<vart::TensorBuffer*> &inputs,
                              const std::vector<vart::TensorBuffer*> &outputs) {
 
   const auto wIdx = engine_.get_my_worker_id();
-  std::cout << "Copy inputs.." << std::endl;
-  std::cout << "wIDX" << wIdx << std::endl;
+  LOG_IF(INFO, ENV_PARAM(DEBUG_DPU_CONTROLLER)) << "Copy inputs.." ;
   // Copy inputs to Device
   copyInputs(wIdx, inputs);
 
   // Execute Kernel
- std::cout << "graph run...\n";
- pl_ctrl_sw_.enqueueGraphRun(1);
- pl_ctrl_sw_.enqueueRuntimeControl();
- pl_ctrl_sw_.enqueueGraphEnd();
+  LOG_IF(INFO, ENV_PARAM(DEBUG_DPU_CONTROLLER)) << "graph run...";
+  pl_ctrl_sw_.enqueueGraphRun(1);
+  pl_ctrl_sw_.enqueueRuntimeControl();
+  pl_ctrl_sw_.enqueueGraphEnd();
 
- const uint64_t aieBase = 0x20000000000;
- //int rc_run = pl_ctrl_sw_.run(aieBase);
- pl_ctrl_sw_.run(aieBase);
-    // wait done;
- pl_ctrl_sw_.wait(0);
-  std::cout << "Graph wait done" << std::endl;
+  const uint64_t aieBase = 0x20000000000;
+  pl_ctrl_sw_.run(aieBase);
+  // Blocking call
+  pl_ctrl_sw_.wait(0);
+  LOG_IF(INFO, ENV_PARAM(DEBUG_DPU_CONTROLLER)) << "Graph wait done" ;
 
   // Copy outputs from Device
   copyOutputs(wIdx, outputs);
