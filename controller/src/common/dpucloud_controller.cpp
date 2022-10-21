a5feb7029e1d26dd77bf067112fecca8036dc298
Gao Yue <gaoyue@xilinx.com>
2022-10-21 15:50:06 +0800

    [fix ]compiler error for xclregread MR

--- controller/src/common/dpucloud_controller.cpp
+++ controller/src/common/dpucloud_controller.cpp
@@ -1521,10 +1521,6 @@ void DpuCloudController::run(const std::vector<vart::TensorBuffer*> &inputs,
           auto size = std::get<1>(out);
           auto reg_id = std::get<3>(out);
           auto data = std::make_unique<char[]>(size);
-<<<<<<< HEAD
-=======
-          //for (unsigned i=0; i < io_bufs.size(); i++) {
->>>>>>> fix bug in debug dump mode dump error
           if(check_exist(reg_id, model_->get_output_regid())) {
             for (auto it :  xdpu_total_dpureg_map_io ) {
               auto regid = std::get<0>(it);
