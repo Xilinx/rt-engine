f50d032cbb2a10b2dedc981c9d3f0a6ea7fdf9c0
Gao Yue <gaoyue@xilinx.com>
2023-05-24 11:05:36 +0800

    expend read range to 0x500

--- device/src/device_handle.cpp
+++ device/src/device_handle.cpp
@@ -213,7 +213,7 @@ XrtNativeContext::XrtNativeContext(XrtNativeDeviceHandle &handle) : handle_(hand
   auto hwctx = xrt::hw_context(device_, handle.get_uuid());
   kernel_ = xrt::kernel(hwctx, handle.get_device_info().kernel_name);
 
-  xrt::set_read_range(kernel_, 0x10, 0x1f0);
+  xrt::set_read_range(kernel_, 0x10, 0x500);
 }
 
 XrtNativeContext::~XrtNativeContext() {
